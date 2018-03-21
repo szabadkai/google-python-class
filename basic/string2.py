3113c3a46b359df3aab55dd03fbab49266e5f44d
adamkaroczi <adamk@roczi.hu>
2018-03-21 23:41:57 +0100

    string2/1

--- basic/string2.py
+++ basic/string2.py
@@ -16,8 +16,12 @@
 # If the string length is less than 3, leave it unchanged.
 # Return the resulting string.
 def verbing(s):
-    # +++your code here+++
-    return
+    if len(s) >= 3:
+        if s[len(s)-3:] == 'ing':
+            s += 'ly'
+        else:
+            s += 'ing'
+    return s
 
 
 # E. not_bad
